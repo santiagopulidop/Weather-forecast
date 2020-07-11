6bc92d84509fb8c11c6ab4dd7a88d727139c36b2
Santiago Pulido Pelaez <santiagopulidopelaez@gmail.com>
2020-07-10 20:26:46 -0500

    colores_

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="p-3 font-color-b">
+      Made by: Santiago Pulido Peláez | &copy;2020
+    </footer>
+  );
+};
+
+export default Footer;
